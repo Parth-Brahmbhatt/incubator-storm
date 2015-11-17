edb495af324dfb8229b0f50c164cb9066fd0c94d
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2015-11-16 16:11:38 -0800

    Removing unwanted test files.

--- external/storm-hive/src/test/java/org/apache/storm/hive/bolt/Test.java
+++
@@ -1,7 +0,0 @@
-package org.apache.storm.hive.bolt;
-
-/**
- * Created by pbrahmbhatt on 11/11/15.
- */
-public class Test {
-}
--- external/storm-kafka/src/jvm/storm/kafka/KafkaSpoutTest.java
+++
@@ -1,7 +0,0 @@
-package storm.kafka;
-
-/**
- * Created by pbrahmbhatt on 11/11/15.
- */
-public class KafkaSpoutTest {
-}
